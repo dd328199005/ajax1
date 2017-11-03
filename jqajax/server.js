25fda8ed3967f5647eb0ee964c309ad11f9ab1c3
daskys <az331127@163.com>
2017-11-04 01:16:37 +0800

    add

---
+++ jqajax/server.js
@@ -0,0 +1,37 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var path = require('path')
+
+http.createServer(function(req, res){
+
+  var pathObj = url.parse(req.url, true)
+  console.log(pathObj)
+
+  switch (pathObj.pathname) {
+    case '/loadMore':
+
+      var curIdx = pathObj.query.idx
+      var len = pathObj.query.len
+      var data = []
+
+      for(var i = 0; i < len; i++) {
+        data.push('内容' + (parseInt(curIdx) + i))
+      }
+      res.end(JSON.stringify(data))
+      console.log(data)
+          
+      break;
+
+    default:
+     fs.readFile(path.join(__dirname ,'static', pathObj.pathname), function(err, data){
+      if(err){
+        res.writeHead(404,'not found')
+        res.end()
+      }else{
+        res.end(data)
+      }
+     })
+      
+  }
+}).listen(8080)
